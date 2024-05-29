7da4b9ee8209fbbdb1c5840cf395025a9f470872
apkhoror <sasukesenda@gmail.com>
2024-05-29 20:59:41 +0700

    web statis aja dulu

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
 let main = document.querySelector("main");
+const cari = document.querySelector(".search");
 
-function toggleSearch() {
+cari.addEventListener("click", () => {
   let fokus = document.querySelector(".kotak-pencarian form input");
 
   main.classList.toggle("search-on");
@@ -9,10 +10,10 @@ function toggleSearch() {
     main.classList.remove("menu-on");
   }
 
-  if (main.classList.contains("search-on")) {
-    fokus.focus();
-  }
-}
+  // if (main.classList.contains("search-on")) {
+  //   fokus.focus();
+  // }
+});
 
 function toggleMenu() {
   if (main.classList.contains("search-on")) {
@@ -20,3 +21,14 @@ function toggleMenu() {
   }
   main.classList.toggle("menu-on");
 }
+
+// Fitur Pencarian
+const searching = document.querySelector("#cari");
+
+searching.addEventListener("click", (e) => {
+  const input = document.getElementById("inputCari").value.toLowerCase();
+  // || item.deskripsi.toLowerCase().includes(input)
+  window.location.href = `pengembangan.html`;
+  // tampilkanHasilnya(results);
+  e.preventDefault();
+});
