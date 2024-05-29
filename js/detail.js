c60762690916fabeaee6d001cea5667f172380e3
apkhoror <sasukesenda@gmail.com>
2024-05-29 21:29:57 +0700

    penyesuaian

--- js/detail.js
+++ js/detail.js
@@ -4,6 +4,6 @@ const namaMakanan = document.getElementById("bakso");
 const pesan = `Hallo om, saya mau pesan ${namaMakanan.innerText}`;
 
 beli.addEventListener("click", () => {
-  window.location.href = `http://wa.me/6283894349952?text="${encodeURIComponent(pesan)}"`;
+  window.location.href = `http://wa.me/6283894349952?text=${encodeURIComponent(pesan)}`;
   //   window.open = `http://wa.me/6283894349952?text=${encodeURIComponent(pesan)}`;
 });
